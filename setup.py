a315afcaa50f2767bb5e1261c1f0b8ef21df7ea0
Tzur Paldi <tzur.paldi@outlook.com>
2024-03-19 10:44:40 +0200

    Add license

--- setup.py
+++ setup.py
@@ -1,21 +1,24 @@
-from setuptools import find_packages, setup
+from setuptools import setup
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
 
 setup(
     name='pytest-cleanuptotal',
     packages=['pytest_cleanuptotal'],
     package_dir={'pytest_cleanuptotal': 'src/pytest_cleanuptotal'},
-    version='0.2',
+    version='0.2.1',
     author='Tzur Paldi',
     author_email='tzur.paldi@outlook.com',
     maintainer='Tzur Paldi',
     maintainer_email='tzur.paldi@outlook.com',
-    license='GNU',
+    license='MIT',
     url='https://github.com/tzurp/pytest_cleanuptotal',
     description='A cleanup plugin for pytest',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Framework :: Pytest",
     ],
     entry_points={
