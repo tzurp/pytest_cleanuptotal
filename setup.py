b08d910d27abfc16ed56a1c6ffa7a5901c81b7a4
tzurp <tzur.paldi@outlook.com>
2025-07-22 15:33:50 +0300

    setup fix

--- setup.py
+++ setup.py
@@ -17,21 +17,16 @@ setup(
         "Documentation": "https://github.com/tzurp/pytest_cleanuptotal#readme",
         "Source Code": "https://github.com/tzurp/pytest_cleanuptotal"
     },
-    classifiers=[
-        "License :: OSI Approved :: Apache Software License",
-        # You can add other classifiers like OS, Python version, etc.
-    ],
-    license_files=["LICENSE"]
+    license_files=["LICENSE"],
     maintainer='Tzur Paldi',
     maintainer_email='tzur.paldi@outlook.com',
-    license='MIT',
-    url='https://github.com/tzurp/pytest_cleanuptotal',
     description='A cleanup plugin for pytest',
     long_description=long_description,
     long_description_content_type='text/markdown',
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Framework :: Pytest",
+        "License :: OSI Approved :: Apache Software License",
     ],
     entry_points={
         'pytest11': [
